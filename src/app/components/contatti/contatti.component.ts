8cfc071f5d01314d0bddaef2fa03311a1b91e233
Francesco Leuzzi <fleuzzi97@gmail.com>
2022-10-17 23:16:34 +0200

    get data from DB

--- src/app/components/contatti/contatti.component.ts
+++ src/app/components/contatti/contatti.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/servizi/firebase.service';
+
 
 @Component({
   selector: 'app-contatti',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contatti.component.scss']
 })
 export class ContattiComponent implements OnInit {
-
-  constructor() { }
+  persone: any;
+  constructor(private firebase: FirebaseService) { }
 
   ngOnInit(): void {
+    this.firebase.getPersone('https://corso-angular-69a9b-default-rtdb.europe-west1.firebasedatabase.app/persone.json')
+    .subscribe((data: any) => {
+      console.log(data)
+      this.persone = Object.keys(data).map((key) => { return data[key]})
+      console.log(this.persone)
+    })
   }
 
 }
--- src/app/servizi/firebase.service.ts
+++ src/app/servizi/firebase.service.ts
@@ -11,4 +11,8 @@ export class FirebaseService {
   insertPersona(url: string, body: {}){
     return this.http.post(url, body)
   }
+
+  getPersone(url: string) {
+    return this.http.get(url)
+  }
 }
